543c0d8ad1c39359e4644335585a32f8653809bc
aymanjameel60 <95094618+aymanjameel60@users.noreply.github.com>
2026-09-07 13:29:11 +0300

    Add native Android iOS scaffolding and Flutter CI

---
+++ android/app/build.gradle.kts
@@ -0,0 +1,31 @@
+plugins {
+    id("com.android.application")
+    id("kotlin-android")
+    id("dev.flutter.flutter-gradle-plugin")
+}
+
+android {
+    namespace = "com.spike.customer"
+    compileSdk = flutter.compileSdkVersion
+    ndkVersion = flutter.ndkVersion
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+    kotlinOptions { jvmTarget = JavaVersion.VERSION_17.toString() }
+
+    defaultConfig {
+        applicationId = "com.spike.customer"
+        minSdk = flutter.minSdkVersion
+        targetSdk = flutter.targetSdkVersion
+        versionCode = flutter.versionCode
+        versionName = flutter.versionName
+    }
+
+    buildTypes {
+        release { signingConfig = signingConfigs.getByName("debug") }
+    }
+}
+
+flutter { source = "../.." }
